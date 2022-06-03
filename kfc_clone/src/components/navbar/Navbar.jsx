e9b8e9c57afd27ad055c9efc2ae295008a1746f0
Partha Pratim Das <dasparthapratim02@gmail.com>
2022-06-04 01:02:54 +0530

    q

--- kfc_clone/src/components/navbar/Navbar.jsx
+++ kfc_clone/src/components/navbar/Navbar.jsx
@@ -1,10 +1,25 @@
+import "./nav.css";
+import {Link} from "react-router-dom"
 export const Navbar = () => {
-    return <>
-        <div className="nav_main">
-            
-    </div>
-    
-    
-    
+  return (
+    <>
+      <div className="nav_main">
+        <div className="left_side">
+                  <img className="logo_img" src="/logo.svg" alt="" />
+                
+          <b> Menu</b>
+          <b>Deals</b>
+        </div>
+        <div className="right_side">
+          <span>
+            <img src="/login2.png" alt="" />
+          </span>
+          <b>signup</b>
+
+                  <img className="cart_img" src="/cart.svg" alt="" />
+                 
+        </div>
+      </div>
     </>
-}
+  );
+};
