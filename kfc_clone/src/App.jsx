53375eafbd971dc5b45d93b2268282bd5107a7a2
Partha Pratim Das <dasparthapratim02@gmail.com>
2022-06-04 01:14:25 +0530

    b

--- kfc_clone/src/App.jsx
+++ kfc_clone/src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
 import { Navbar } from './components/navbar/Navbar'
+import { Home } from './components/home/Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-     <Navbar></Navbar>
+      <Navbar></Navbar>
+      <Home></Home>
     </div>
   )
 }
--- kfc_clone/src/components/home/Home.jsx
+++ kfc_clone/src/components/home/Home.jsx
@@ -1,6 +1,10 @@
+import "./home.css"
+
 export const Home = () => {
     return <>
-    
+        <div className="black">
+            <b>LET'S ORDER FOR DELIVERY, PICK UP, OR DINE-IN</b>
+    </div>
     
     
     
