2c282cc590d317b4af47e3bcde899806b43156a7
Partha Pratim Das <dasparthapratim02@gmail.com>
2022-06-06 01:25:00 +0530

    b

--- kfc_clone/src/components/menu/Menu.jsx
+++ kfc_clone/src/components/menu/Menu.jsx
@@ -1,5 +1,23 @@
+import "./menu.css";
+
 export const Menu = () => {
-    return <>
-    <h1>menu page</h1>
+  return (
+    <>
+      <div className="menu_list">
+        <img src="./band.png" alt="" />
+        <h3>KFC MENU</h3>
+        <ul>
+          <li>KFC BIG TREAT WEEK</li>
+          <li>NEW CHICKEN ROLLS</li>
+          <li>HOT DEALS</li>
+          <li>CHICKEN BUCKETS</li>
+          <li>BOX MEALS</li>
+          <li>BURGERS</li>
+          <li>BIRYANI BUCKETS</li>
+          <li>SNACK</li>
+          <li>BEVERAGES & DESSERTS</li>
+        </ul>
+      </div>
     </>
-}
+  );
+};
